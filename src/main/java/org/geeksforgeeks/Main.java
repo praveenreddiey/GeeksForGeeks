ffb8efd1bef7b4a35ebf260e5254e22142952b46
praveenreddiey <praveenreddiey@gmail.com>
2022-11-03 21:18:30 +0530

    just goofing around again

--- src/main/java/org/geeksforgeeks/Main.java
+++ src/main/java/org/geeksforgeeks/Main.java
@@ -1,9 +1,7 @@
 package org.geeksforgeeks;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    }}
+    }
+}
